f1c4a6e46ee6535664f42933e32b6d336cb42c45
Egor Klenin <e.g2gor@gmail.com>
2021-03-16 16:08:42 +0300

    Add: static invoice template
    feature-0001/implement-invoice-pugjs-template

--- api/controllers/invoice.js
+++ api/controllers/invoice.js
@@ -1,14 +1,28 @@
 'use strict'
-
+const pug = require('pug')
+const fs = require('fs-extra')
+const path = require('path')
 const invoiceService = require('../services/invoice')
 
 class invoiceController {
 
-  async requestInvoice() {
+  /**
+
+   * @param {*} req 
+   * @param {*} res 
+   */
+  static async requestInvoice(req,res) {
     // TODO: log request to db
     // TODO: shedule pdf generation
     // TODO: shedule mail
-
+    try {
+      const html = pug.renderFile('./api/views/invoice/invoice.pug', {
+        logo: Buffer.from(fs.readFileSync('./api/views/invoice/logo.webp')).toString('base64'),
+      })
+      res.send(html);
+    } catch (err) { 
+      console.log(err); 
+    }
   }
 }
 
--- api/routes/invoice.js
+++ api/routes/invoice.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 const invoiceController = require('../controllers/invoice')
 
-/* GET users listing. */
-router.get('/', async (req, res) => {
-  // TODO: use invoice controller
-  res.send('Try to send invoice and respond with status');
-});
+/* Generate invoice */
+router.get('/', invoiceController.requestInvoice)
 
 module.exports = router;
