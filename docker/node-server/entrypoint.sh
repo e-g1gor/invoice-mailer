64a724a509d47a2c1215f616008f597f1a76a0df
Egor Klenin <e.g2gor@gmail.com>
2021-03-19 16:22:28 +0300

    Fix: make chromium installation in container cachable

--- docker/node-server/entrypoint.sh
+++ docker/node-server/entrypoint.sh
@@ -3,6 +3,10 @@
 #  Refresh dependencies
 npm install
 
+which chromium
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+PUPPETEER_EXECUTABLE_PATH=$(which chromium)
+
 echo "Starting node server."
 
 node ./bin/www
